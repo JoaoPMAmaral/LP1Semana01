09230da4b586cfe6ed4fb42849be6067f50b8aba
Joao A <joao.pedro.matias.amaral@gmail.com>
2026-02-19 16:02:13 +0000

    added float, double, bools, hadnt read so i had been pushing since the beginning

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -9,6 +9,8 @@ namespace VariousTypes
         private static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
+
+            //1-4
             int i = 1;
             short s = 2;
             byte b = 3;
@@ -20,11 +22,27 @@ namespace VariousTypes
             Console.WriteLine(b);
             Console.WriteLine(l);
             
+            //5-7
             char emoji = '\u2606';
-            
-            
+
             Console.WriteLine("Emoji");
             Console.WriteLine(emoji);
+
+            //8-10
+            float f = 3.1416f;
+            double d = 3.1416;
+
+            Console.WriteLine("Float e double");
+            Console.WriteLine(f);
+            Console.WriteLine(d);
+
+            //11-14
+            bool t = true;
+            bool fal = false;
+
+            Console.WriteLine("Bools");
+            Console.WriteLine(t);
+            Console.WriteLine(fal);
         }
     }
 }

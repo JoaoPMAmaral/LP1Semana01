1447152667ff2ee1caafd9499f5793f17c6fb832
Joao A <joao.pedro.matias.amaral@gmail.com>
2026-02-19 15:43:09 +0000

    updated solution, added the other project 'VariousTypes'

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int i = 1;
+            short s = 2;
+            byte b = 3;
+            long l = 4;
+
+            Console.WriteLine("Valores das variaveis int,short,byte,long, respetivamente");
+            Console.WriteLine(i);
+            Console.WriteLine(s);
+            Console.WriteLine(b);
+            Console.WriteLine(l);
+        }
+    }
+}

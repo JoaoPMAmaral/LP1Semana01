64353046046fea2b6bd594d8105c3e046f74441f
Joao A <joao.pedro.matias.amaral@gmail.com>
2026-02-19 16:35:26 +0000

    added e01, for the third and last time, hopefully

--- E01/Program.cs
+++ E01/Program.cs
@@ -6,7 +6,7 @@ namespace E01
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Unicode for Registered trademark: u00AE");
+            Console.WriteLine("Hello LP!");
         }
     }
 }
